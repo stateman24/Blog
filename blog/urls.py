f43b9896379b6d2f6b6e2d20afb04bcb047580fb
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2024-03-19 19:59:33 +0100

    created a pagiantion feature

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from . import views
+from .views import post_list, post_detail
 
 #namespace for the app 'blog'
 app_name = 'blog'
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('', post_list, name='post_list'),
+    path('<slug:post>/', post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # To list all publised posts 
 def post_list(request):
-    post = Post.published.all()
-    context = {'post':post}
-    render(request, 'blog/post/list.html', context)
+    posts_list = Post.published.all()
+    #paginate list of 3
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts= paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page is not an integer
+        posts = paginator.page(1)
 
-def post_detail(request, id):
-    post = get_list_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    context = {'post':post}
-    render(request, 'blog/post/detail.html', context)
+    except EmptyPage:
+        # if page number is out of the last page result
+        posts= paginator.page(paginator.num_pages)
+
+    context = {'posts':posts,}
+    return render(request, 'blog/post/list.html', context )
+
+# render post details
+def post_detail(request, post):
+    post = get_object_or_404(Post,  
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+    )
+
+    context = {'post':post,}
+    return render(request, 'blog/post/detail.html', context)
